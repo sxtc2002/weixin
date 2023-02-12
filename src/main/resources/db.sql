6285ff9f295fd657d6beace65ac56f3f44d4740b
sxtc2002 <2499948209@qq.com>
2023-02-12 22:58:29 +0800

    修改数据库属性

--- src/main/resources/db.sql
+++ src/main/resources/db.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS `Users` (
-    `id` char(22) NOT NULL,
+    `id` char(50) NOT NULL,
     `qualification` boolean NOT NULL DEFAULT '0',
     `createdAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (`id`)
