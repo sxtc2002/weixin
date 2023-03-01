ec5df552f1cbef87a8a837515658b90b0c3f3519
sxtc2002 <2499948209@qq.com>
2023-03-01 23:14:59 +0800

    修改数据库

---
+++ src/main/java/com/tencent/wxcloudrun/model/Administrator.java
@@ -0,0 +1,10 @@
+package com.tencent.wxcloudrun.model;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class Administrator implements Serializable {
+    private String id;
+}
