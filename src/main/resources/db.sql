772dff9b1ab767d56eaf9c7e0853bda16171d498
sxtc2002 <2499948209@qq.com>
2023-03-05 15:04:30 +0800

    修改sql语句

--- src/main/resources/db.sql
+++ src/main/resources/db.sql
@@ -1,7 +1,6 @@
 CREATE TABLE if not exists Users
 (
     `id` char(50) NOT NULL,
-    `ifjoin` boolean NOT NULL DEFAULT '0',
     `hit` int NOT NULL DEFAULT '0',
     `nick` char(50),
     `tel` char(50),
