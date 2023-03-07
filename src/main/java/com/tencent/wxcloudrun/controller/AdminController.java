880eb6624bc2db93f30d5a34b0accb466ceda4e2
sxtc2002 <2499948209@qq.com>
2023-03-07 21:16:49 +0800

    添加admin日志

--- src/main/java/com/tencent/wxcloudrun/controller/AdminController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/AdminController.java
@@ -7,6 +7,9 @@ import com.tencent.wxcloudrun.model.User;
 import com.tencent.wxcloudrun.service.AdminService;
 import com.tencent.wxcloudrun.service.PrizeService;
 import com.tencent.wxcloudrun.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -20,11 +23,13 @@ public class AdminController {
     final AdminService adminService;
     final PrizeService prizeService;
     final UserService userService;
+    final Logger logger;
 
-    public AdminController(AdminService adminService, PrizeService prizeService, UserService userService) {
+    public AdminController(@Autowired AdminService adminService, PrizeService prizeService, UserService userService) {
         this.adminService = adminService;
         this.prizeService = prizeService;
         this.userService = userService;
+        this.logger = LoggerFactory.getLogger(UserController.class);
     }
 
     private boolean verify(String id) {
@@ -43,6 +48,7 @@ public class AdminController {
 
     @PostMapping(value = "/api/draw")
     ApiResponse draw(@RequestHeader("x-wx-openid") String id) {
+        logger.info("/api/draw post request");
         if(!verify(id)) {
             return ApiResponse.error("没有权限");
         }
@@ -68,6 +74,7 @@ public class AdminController {
     }
     @GetMapping(value = "/api/draw/view")
     ApiResponse viewWinners(@RequestHeader("x-wx-openid") String id) {
+        logger.info("/api/draw/view get request");
         if(!verify(id)) {
             return ApiResponse.error("没有权限");
         }
