5b01ac22cedd99fed61286e4b29ebee77c35f476
sxtc2002 <2499948209@qq.com>
2023-05-07 16:19:25 +0800

    添加获取素材列表接口

--- src/main/java/com/tencent/wxcloudrun/controller/ItemController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/ItemController.java
@@ -1,7 +1,9 @@
 package com.tencent.wxcloudrun.controller;
 
 import com.tencent.wxcloudrun.config.ApiResponse;
+import com.tencent.wxcloudrun.dto.BatchRequest;
 import com.tencent.wxcloudrun.dto.ItemRequest;
+import com.tencent.wxcloudrun.model.Batch;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
@@ -16,11 +18,19 @@ public class ItemController {
         this.logger = LoggerFactory.getLogger(ItemController.class);
     }
     @PostMapping(value = "/api/item")
-    ApiResponse item2(@RequestHeader("x-wx-openid") String id, @RequestBody ItemRequest itemRequest) {
+    ApiResponse item(@RequestHeader("x-wx-openid") String id, @RequestBody ItemRequest itemRequest) {
         logger.info("/api/item get request, id: {}", id);
         String url = "https://api.weixin.qq.com/cgi-bin/material/get_material";
         RestTemplate restTemplate = new RestTemplate();
         String item = restTemplate.postForObject(url, itemRequest, String.class);
         return ApiResponse.ok(item);
+    }
+    @PostMapping(value = "/api/batch")
+    ApiResponse batch(@RequestHeader("x-wx-openid") String id, @RequestBody BatchRequest batchRequest) {
+        logger.info("/api/batch get request, id: {}", id);
+        String url = "https://api.weixin.qq.com/cgi-bin/material/batchget_material";
+        RestTemplate restTemplate = new RestTemplate();
+        Batch batch = restTemplate.postForObject(url, batchRequest, Batch.class);
+        return ApiResponse.ok(batch);
     }
 }
---
+++ src/main/java/com/tencent/wxcloudrun/model/Batch.java
@@ -0,0 +1,10 @@
+package com.tencent.wxcloudrun.model;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class Batch implements Serializable {
+    private int total_count;
+}
