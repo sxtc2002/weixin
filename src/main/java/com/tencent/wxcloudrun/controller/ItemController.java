ff8d7e4efb61f42e1a23e6fcc054010596621e31
sxtc2002 <2499948209@qq.com>
2023-05-07 15:10:02 +0800

    调试

--- src/main/java/com/tencent/wxcloudrun/controller/ItemController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/ItemController.java
@@ -3,6 +3,7 @@ package com.tencent.wxcloudrun.controller;
 import com.tencent.wxcloudrun.config.ApiResponse;
 import com.tencent.wxcloudrun.dto.ItemRequest;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -10,14 +11,10 @@ import org.springframework.web.client.RestTemplate;
 public class ItemController {
 
     @GetMapping(value = "/api/item")
-    ApiResponse item() {
+    ApiResponse item(@RequestBody ItemRequest itemRequest) {
         String url = "https://api.weixin.qq.com/cgi-bin/material/batchget_material";
         RestTemplate restTemplate = new RestTemplate();
-        ItemRequest request = new ItemRequest();
-        request.setType("news");
-        request.setOffset(0);
-        request.setCount(20);
-        String item = restTemplate.postForObject(url, request, String.class);
+        String item = restTemplate.postForObject(url, itemRequest, String.class);
         return ApiResponse.ok(item);
     }
 }
