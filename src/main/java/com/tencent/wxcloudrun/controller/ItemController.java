53afed3890ab6482d4bbb95e3b2587ac4d4afcec
sxtc2002 <2499948209@qq.com>
2023-05-07 15:32:22 +0800

    调试

--- src/main/java/com/tencent/wxcloudrun/controller/ItemController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/ItemController.java
@@ -2,16 +2,26 @@ package com.tencent.wxcloudrun.controller;
 
 import com.tencent.wxcloudrun.config.ApiResponse;
 import com.tencent.wxcloudrun.dto.ItemRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class ItemController {
 
+    final Logger logger;
+
+    public ItemController() {
+        this.logger = LoggerFactory.getLogger(ItemController.class);
+    }
+
     @GetMapping(value = "/api/item")
-    ApiResponse item(@RequestBody ItemRequest itemRequest) {
+    ApiResponse item(@RequestHeader("x-wx-openid") String id, @RequestBody ItemRequest itemRequest) {
+        logger.info("/api/item get request, id: {}", id);
         String url = "https://api.weixin.qq.com/cgi-bin/material/batchget_material";
         RestTemplate restTemplate = new RestTemplate();
         String item = restTemplate.postForObject(url, itemRequest, String.class);
