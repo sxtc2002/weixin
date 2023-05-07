1c84940d245371823c3d0b888f0a4c3e23fc9006
sxtc2002 <2499948209@qq.com>
2023-05-07 13:08:35 +0800

    添加获取素材接口

---
+++ src/main/java/com/tencent/wxcloudrun/controller/ItemController.java
@@ -0,0 +1,25 @@
+package com.tencent.wxcloudrun.controller;
+
+import com.tencent.wxcloudrun.config.ApiResponse;
+import com.tencent.wxcloudrun.model.Item;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class ItemController {
+
+    @GetMapping(value = "/api/item")
+    ApiResponse item() {
+        RestTemplate restTemplate = new RestTemplate();
+        MultiValueMap<String, Object> paramMap = new LinkedMultiValueMap<>();
+        paramMap.add("type", "news");
+        paramMap.add("offset", "0");
+        paramMap.add("count", "20");
+        Item item = restTemplate.postForObject("https://api.weixin.qq.com/cgi-bin/material/batchget_material"
+                , paramMap, Item.class);
+        return ApiResponse.ok(item);
+    }
+}
---
+++ src/main/java/com/tencent/wxcloudrun/model/Item.java
@@ -0,0 +1,10 @@
+package com.tencent.wxcloudrun.model;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class Item implements Serializable {
+    private int total_count;
+}
